cb73da4b3f762a89b20167efbd536fce9327a0c8
Chrisw131 <walkerchris0523@gamil.com>
2024-11-13 14:57:01 -0500

    Lab303.5.1

---
+++ src/LoopDemoOne.java
@@ -0,0 +1,21 @@
+// For Loops - allows us to iterate over items until a specified condition is met or satisfied.
+
+// EX- 
+//for (initialization;termination;increment) {
+//    statement(s)
+//}
+
+
+public class LoopDemoOne {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		 int n = 5;
+		    // for loop  
+		    for (int i = 1; i <= n; ++i) {
+		      System.out.println("Java is fun");
+		    }
+		  }
+
+
+	}
